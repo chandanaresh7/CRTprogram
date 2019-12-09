967ea7e066c4fe587d94c8849255d54e308b479e
chandanaresh7 <58408694+chandanaresh7@users.noreply.github.com>
2019-12-09 10:26:45 +0530

    Add files via upload

---
+++ leftshiftrightshift.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+int a=10,b=10;
+printf("a<<2=%d",a<<2);
+printf("b>>2=%d",b>>2);
+//left shif:
+//shifting the bits on to the left side
+//and assining 0'sn to the right side
+return 0;
+}
